a48dedb4697c9cff408a0e77fd804596adfc7e63
Francisco De la Fuente <francisco00@gmail.com>
2026-01-26 11:07:06 -0300

    security improving

--- Dockerfile
+++ Dockerfile
@@ -1,38 +1,27 @@
-# Etapa 1: Build
+# ---------- Build ----------
 FROM node:20-alpine AS builder
-
-# Crear directorio de la app
 WORKDIR /app
 
-# Copiar package.json y lock
 COPY package*.json ./
-# Si usas bun.lockb o pnpm, copia también el lock correspondiente
-RUN npm install
+RUN npm ci
 
-# Copiar todo el proyecto
 COPY . .
-
-
-
-# Build de producción
 RUN npm run build
 
-# Etapa 2: Run
+# ---------- Runtime ----------
 FROM node:20-alpine
-
 WORKDIR /app
 
-# Copiar solo lo necesario de build
+ENV NODE_ENV=production
+
 COPY --from=builder /app/package*.json ./
 COPY --from=builder /app/node_modules ./node_modules
 COPY --from=builder /app/.next ./.next
 COPY --from=builder /app/public ./public
 COPY --from=builder /app/next.config.mjs ./next.config.mjs
-COPY .env.local .env
 
-# Puerto en el que Next.js escuchará
-ENV PORT=4500
-EXPOSE 4500
+# Cloud Run escucha en $PORT (normalmente 8080)
+EXPOSE 8080
 
-# Comando para iniciar la app
-CMD ["npm", "run", "start", "--", "-p", "4500"]
+CMD ["npm", "run", "start"]
+
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  poweredByHeader: false,
+
   images: {
-    minimumCacheTTL: 2678400 * 6, // 3 months
+    minimumCacheTTL: 2678400 * 6, // 6 meses
     remotePatterns: [
       {
         protocol: 'https',
@@ -30,6 +32,53 @@ const nextConfig = {
       },
     ],
   },
-}
 
-export default nextConfig
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "geolocation=(), microphone=(), camera=()",
+          },
+          /*{
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+          {
+            key: "Cross-Origin-Resource-Policy",
+            value: "same-origin",
+          },*/
+          /*{
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; img-src 'self' https: data:; script-src 'self'; style-src 'self' 'unsafe-inline'",
+          },*/
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
